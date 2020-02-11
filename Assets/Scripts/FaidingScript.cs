19f0922851f853534b82c8abd4b8dc0fa3333058
mbmt <mbmt@cin.ufpe.br>
2020-02-11 17:36:23 -0300

    Sound effects while shooting and hitting target

---
+++ Assets/Scripts/FaidingScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaidingScript : MonoBehaviour
+{
+    public float animationTime;
+    public float speed;
+    private float currentTime;
+    void Start() {
+        this.currentTime += Time.deltaTime;
+    }
+    
+    void Update()
+    {
+        this.currentTime += Time.deltaTime;
+        if (this.currentTime>=this.animationTime) {
+            Destroy(this.gameObject);
+        }
+        transform.position += ((Vector3.down * speed * Time.deltaTime *0.5f)+(Vector3.left *speed*Time.deltaTime *Random.Range(-2.0f,2.0f)));
+        transform.localScale = transform.localScale * 0.99f;
+    }
+}
--- Assets/Scripts/InvaderScript.cs
+++ Assets/Scripts/InvaderScript.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class InvaderScript : MonoBehaviour
 {
+    public GameObject fadingInvader;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Destroy(collision.gameObject);
+        Instantiate(this.fadingInvader,this.transform.position,this.transform.rotation);
         Destroy(this.gameObject);
+
     }
 }
