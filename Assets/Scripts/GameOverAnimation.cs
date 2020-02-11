49b20b9879678851d54408429ef0af6d748b7ce4
mbmt <mbmt@cin.ufpe.br>
2020-02-11 15:40:18 -0300

    Created Game Over Screen

---
+++ Assets/Scripts/GameOverAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverAnimation : MonoBehaviour
+{
+    public float screenTime;
+    public GameObject sceneManager;
+
+    private float currentScreenTime;
+
+    private void Start()
+    {
+        this.sceneManager = GameObject.Find("SceneManager");
+        this.currentScreenTime = 0;
+    }
+    void Update()
+    {
+        this.currentScreenTime += Time.deltaTime;
+        if (this.currentScreenTime >= this.screenTime) {
+            this.sceneManager.GetComponent<SceneManagerScript>().GoToMenu();
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/SceneManagerScript.cs
+++ Assets/Scripts/SceneManagerScript.cs
@@ -8,6 +8,7 @@ public class SceneManagerScript : MonoBehaviour
     public GameObject canvas;
     public GameObject player;
     public GameObject enemys;
+    public GameObject gameOverScreen;
 
     private GameObject currentPlayer;
     private GameObject currentEnemys;
@@ -16,13 +17,17 @@ public class SceneManagerScript : MonoBehaviour
     {
         this.canvas.SetActive(false);
         this.currentPlayer = Instantiate(this.player, Vector3.up*0.5f, Quaternion.identity);
-        this.currentEnemys = Instantiate(this.enemys, Vector3.up*5f, Quaternion.identity);
+        this.currentEnemys = Instantiate(this.enemys, Vector3.up*3f, Quaternion.identity);
     }
 
     public void EndGame()
     {
         Destroy(this.currentPlayer);
         Destroy(this.currentEnemys);
+        Instantiate(gameOverScreen);
+    }
+
+    public void GoToMenu() {
         this.canvas.SetActive(true);
     }
 }
